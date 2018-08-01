a188f80168462655ec3db4942beff593e6efb54c
nwgquqnu <fillkiller@hotmail.com>
2018-08-01 13:25:26 +0300

    Added two spring boot services

---
+++ cities-storage/src/main/java/com/crfinder/citiesstorage/CitiesStorageApplication.java
@@ -0,0 +1,12 @@
+package com.crfinder.citiesstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitiesStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitiesStorageApplication.class, args);
+	}
+}
